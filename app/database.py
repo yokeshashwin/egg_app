22490d0b6aa701709b4a0cda5f90687cdcffd80d
unknown <hicorelearningapp@gmail.com>
2026-01-08 21:20:20 +0530

    merge remote and local history

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./egg.db"
+DATABASE_URL = "sqlite:///./egg.database"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
